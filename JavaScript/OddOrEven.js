180ff3d30f7cbb41585b87a3240aa2f8f7779daa
Milena Ivanova <milena_ii@abv.bg>
2017-09-27 15:55:19 +0300

    Some html, css, javaScript

---
+++ JavaScript/OddOrEven.js
@@ -0,0 +1,13 @@
+function solve(arg) {
+	var num = +arg[0];
+	if (num % 2 === 0 ){
+	console.log('even' + ' ' + num);
+	} 
+	else {
+		console.log('odd' + ' ' + num);
+	};
+	
+};
+
+// solve([5]);
+
