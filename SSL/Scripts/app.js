5d3783603bb991927bf18f19545e0775023fc4e6
Milena Ivanova <milena_ii@abv.bg>
2017-10-08 12:14:16 +0300

    ok

--- SSL/Scripts/app.js
+++ SSL/Scripts/app.js
@@ -43,7 +43,7 @@ function drawItems() {
 function deleteItem(index) {
 	currentList.items.splice(index, 1);  //remove the item
 	drawItems();                         // see the result
-	}
+}
 
 function checkItem(index) {
 	if ($("#item_" + index).hasClass("checked")) {     //if it is checked, when you push again C
@@ -87,3 +87,60 @@ $(function () {
 	})
 });
 
+$(function () {
+	$.contextMenu({
+		selector: '.context-menu-two',
+		callback: function (key, options) {
+			var m = "clicked: " + key;
+			window.console && console.log(m) || alert(m);
+		},
+		items: {
+			"apples": { name: "Apples" },
+			"pears": { name: "Pears" },
+			"bananas": { name: "Bananas" },
+			"oranges": { name: "Oranges" },
+			"grapes": { name: "Grapes" },
+		}
+	});
+	$('.context-menu-one').on('click', function (e) {
+		console.log('clicked', this);
+	})
+});
+
+$(function () {
+	$.contextMenu({
+		selector: '.context-menu-three',
+		callback: function (key, options) {
+			var m = "clicked: " + key;
+			window.console && console.log(m) || alert(m);
+		},
+		items: {
+			"whole": { name: "Whole grain bread" },
+			"wheat": { name: "Wheat bread" },
+			"corn": { name: "Cornbread" },
+			"dough": { name: "Dough" },
+		}
+	});
+	$('.context-menu-one').on('click', function (e) {
+		console.log('clicked', this);
+	})
+});
+
+$(function () {
+	$.contextMenu({
+		selector: '.context-menu-four',
+		callback: function (key, options) {
+			var m = "clicked: " + key;
+			window.console && console.log(m) || alert(m);
+		},
+		items: {
+			"pork": { name: "Pork" },
+			"chicken": { name: "Chicken" },
+			"veal": { name: "Veal" },
+			"mince": { name: "Mince" },
+		}
+	});
+	$('.context-menu-one').on('click', function (e) {
+		console.log('clicked', this);
+	})
+});
