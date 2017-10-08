230dac6e8989c5834e1ba89d1585e35aea00bc5c
Milena Ivanova <milena_ii@abv.bg>
2017-10-08 14:34:02 +0300

    SPA

--- Button/Button/Scripts/app.js
+++ Button/Button/Scripts/app.js
@@ -16,7 +16,7 @@ function createShoppingList() {
 }
 
 function addItem() {
-	let newItem = {};   //create object
+	var newItem = {};   //create object
 	newItem.name = $("#newItemName").val();
 	currentList.items.push(newItem);
 	console.info(currentList);
@@ -25,27 +25,25 @@ function addItem() {
 }
 
 function drawItems() {
-	let $list = $("#shoppingListItems").empty();  //jquery element
+	var $list = $("#shoppingListItems").empty();  //jquery element
 
-	for (let i = 0; i < currentList.items.length; i++) {
-		let currentItem = currentList.items[i];
-		let $li = $("<li>").html(currentItem.name)
+	for (var i = 0; i < currentList.items.length; i++) {
+		var currentItem = currentList.items[i];
+		var $li = $("<li>").html(currentItem.name)
 			.attr("id", "item_" + i); //attribute -> id, name, curIndex
-		//var $countBtn =
-		//	$("<button onclick='count(" + i + ")'>count</button>").appendTo($li);
-				let $deleteBtn =
-			$("<button onclick='deleteItem(" + i + ")'>Delete</button>").appendTo($li);
-		let $checkBtn =
-			$("<button onclick='checkItem(" + i + ")'>Check</button>").appendTo($li); //do not forget "'"!
+		var $deleteBtn =
+			$("<button onclick='deleteItem(" + i + ")'>D</button>").appendTo($li);
+		var $checkBtn =
+			$("<button onclick='checkItem(" + i + ")'>C</button>").appendTo($li); //do not forget "'"!
+
 		$li.appendTo($list);
 	}                                                           //56 min
 }
 
-
 function deleteItem(index) {
 	currentList.items.splice(index, 1);  //remove the item
 	drawItems();                         // see the result
-}
+	}
 
 function checkItem(index) {
 	if ($("#item_" + index).hasClass("checked")) {     //if it is checked, when you push again C
@@ -67,7 +65,7 @@ $(function () {
 	$.contextMenu({
 		selector: '.context-menu-one',
 		callback: function (key, options) {
-			let m = "clicked: " + key;
+			var m = "clicked: " + key;
 			window.console && console.log(m) || alert(m);
 		},
 		items: {
@@ -82,64 +80,6 @@ $(function () {
 			//		return 'context-menu-icon context-menu-icon-quit';
 			//	}
 			//}
-		}
-	});
-	$('.context-menu-one').on('click', function (e) {
-		console.log('clicked', this);
-	})
-});
-
-$(function () {
-	$.contextMenu({
-		selector: '.context-menu-two',
-		callback: function (key, options) {
-			let m = "clicked: " + key;
-			window.console && console.log(m) || alert(m);
-		},
-		items: {
-			"apples": { name: "Apples" },
-			"pears": { name: "Pears" },
-			"bananas": { name: "Bananas" },
-			"oranges": { name: "Oranges" },
-			"grapes": { name: "Grapes" },
-		}
-	});
-	$('.context-menu-one').on('click', function (e) {
-		console.log('clicked', this);
-	})
-});
-
-$(function () {
-	$.contextMenu({
-		selector: '.context-menu-three',
-		callback: function (key, options) {
-			let m = "clicked: " + key;
-			window.console && console.log(m) || alert(m);
-		},
-		items: {
-			"whole": { name: "Whole grain bread" },
-			"wheat": { name: "Wheat bread" },
-			"corn": { name: "Cornbread" },
-			"dough": { name: "Dough" },
-		}
-	});
-	$('.context-menu-one').on('click', function (e) {
-		console.log('clicked', this);
-	})
-});
-
-$(function () {
-	$.contextMenu({
-		selector: '.context-menu-four',
-		callback: function (key, options) {
-			let m = "clicked: " + key;
-			window.console && console.log(m) || alert(m);
-		},
-		items: {
-			"pork": { name: "Pork" },
-			"chicken": { name: "Chicken" },
-			"veal": { name: "Veal" },
-			"mince": { name: "Mince" },
 		}
 	});
 	$('.context-menu-one').on('click', function (e) {
