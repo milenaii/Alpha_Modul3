063c7a6f5e27231ec896a428df4bb85704c82c74
Milena Ivanova <milena_ii@abv.bg>
2017-12-27 21:29:10 +0200

    уе

--- JavaScript/Sort3numbers.js
+++ JavaScript/Sort3numbers.js
@@ -1,33 +1,34 @@
-function sortThreeNumbers(args){
+function sortThreeNumbers(args) {
     let a = +args[0],
         b = +args[1],
         c = +args[2];
-    
-if (a >= b){
-    if (a >= c){
-        if (b >= c){
-            console.log( a, b, c);
+
+    if (a >= b) {
+        if (a >= c) {
+            if (b >= c) {
+                let sortNums = [a,b,c];
+                console.log(sortNums);
+            }
+            else if (c > b) {
+                console.log(a, c, b);
+            }
         }
-        else if (c > b){
-            console.log( a, c, b);
+        else if (c > a) {
+            console.log(c, a, b);
         }
     }
-    else if (c > a){
-        console.log(c, a, b);
+    else if (b > a) {
+        if (b >= c) {
+            if (a >= c) {
+                console.log(b, a, c);
+            }
+            else if (c > a) {
+                console.log(b, c, a);
+            }
+        }
+        else if (c > b) {
+            console.log(c, b, a);
+        }
     }
 }
-else if (b > a){
-    if (b >= c){
-        if (a >= c){
-            console.log( b, a, c);
-        }
-        else if (c > a){
-            console.log( b, c, a);
-        }
-    }
-    else if (c > b){
-            console.log( c, b, a);
-    }
-}
-}
-sortThreeNumbers(['-2', '-2', '1']);
+sortThreeNumbers(['2', '1', '-1']);
